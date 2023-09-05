00ccfd5dcb3e776937a1b6bc6f35ef9c7f1860c3
raisircl <rai.verma@live.com>
2023-09-05 17:51:39 +0530

    Box class

---
+++ Box.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Box
+    {
+		private int _length;
+
+		public int Length
+		{
+			get { return _length; }
+			set { _length = value; }
+		}
+		private int _breadth;
+
+		public int Breadth
+		{
+			get { return _breadth; }
+			set { _breadth = value; }
+		}
+		private int _height;
+
+		public int Height
+		{
+			get { return _height; }
+			set { _height = value; }
+		}
+		public int area()
+		{
+			return _length * _breadth;
+		}
+		public int vol()
+		{
+			return _length * _breadth * _height;
+		}
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -15,5 +15,21 @@ class Program
         r1.Breadth = 4;
         Console.WriteLine($"Rect1 Dimension is {r1.Length}x{r1.Breadth} and its area is {r1.Area()}");
 
+        Box b1= new Box();  
+        b1.Length = 5;  
+        b1.Breadth = 4;
+        b1.Height = 5;  
+        Console.WriteLine($"Box dimension is {b1.Length}x{b1.Breadth}x{b1.Height}. Area is {b1.area()}. Volume is {b1.vol()} ");
+
     }
 }
+/*
+ BankAcc 
+AccNo - get/set
+AccName  - get/set
+AccBalance - ReadOnly
+ 
+Deposit( int amnt)
+Withdraw(int amnt)
+
+ */
