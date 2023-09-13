9ea0ab0180686a4f55256d1232c74ed76a9b7ec3
raisircl <rai.verma@live.com>
2023-09-13 18:00:05 +0530

    interface

---
+++ IShape.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal interface IShape
+    {
+        void Area();
+    }
+    interface IGreet
+    {
+        void Msg();
+    }
+    class DishaRect : IShape, IGreet
+    {
+        public void Area()
+        {
+            Console.WriteLine("Area of Rect");
+        }
+
+        public void Msg()
+        {
+            Console.WriteLine("Hello User");    
+        }
+    }
+    class Circle : IShape
+    {
+        public void Area()
+        {
+            Console.WriteLine("Area of Circle");
+        }
+    }
+    class IshapeProgram
+    {
+       static void Main() 
+        {
+            IShape shape;
+            DishaRect rect= new DishaRect();
+           
+            shape = rect;
+            shape.Area();
+
+            IGreet greet = rect;
+            greet.Msg();
+
+
+            shape= new Circle();    
+            shape.Area();   
+
+        }
+    }
+}
+/*
+ interface having some functions without body.
+it is just like pure abstract class of c++
+it sign a contract with a class that is "Interface said i will provide you 
+declarations and you will (class) write the definitions"
+
+ 
+ */
--- InheriProgram.cs
+++ InheriProgram.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleApp2
-{
-    internal class InheriProgram
-    {
-        static void Main()
-        {
-            //Quardilateral q1=new Quardilateral(1,2,3,4);
-            //Console.WriteLine(q1.ToString());
-            Square s1=new Square(5);
-            Console.WriteLine($"{s1.ToString()} and area is {s1.Area()}");
+//namespace ConsoleApp2
+//{
+//    internal class InheriProgram
+//    {
+//        static void Main()
+//        {
+//            //Quardilateral q1=new Quardilateral(1,2,3,4);
+//            //Console.WriteLine(q1.ToString());
+//            Square s1=new Square(5);
+//            Console.WriteLine($"{s1.ToString()} and area is {s1.Area()}");
 
-        }
-    }
-}
+//        }
+//    }
+//}
